c85c75384266d885ea58b4d43d31da9a921afdde
mc <20307130112@fudan.edu.cn>
2023-05-05 20:20:18 +0800

    let program can do every test

--- main.c
+++ main.c
@@ -17,9 +17,25 @@
 #include "assem.h"
 #include "assemblock.h"
 #include "treep2assem.h"
+#include "flowgraph.h"
+#include "liveness.h"
+#include "ig.h"
 #include "bg.h"
+#define I(a) AS_InstrList(a, NULL)
+#define IL(a, b) AS_InstrList(a, b)
+#define MI(a, b, c) AS_Move(a, b, c)
+#define LI(a, b) AS_Label(a, b)
+#define OI(a, b, c, d) AS_Oper(a, b, c, d)
+#define T(a) Temp_TempList(a, NULL)
+#define TL(a, b) Temp_TempList(a, b)
+#define LL(a, b) Temp_LabelList(a, b)
+#define L(a) Temp_LabelList(a, NULL)
+#define Targets(a) AS_Targets(a)
 A_prog root;
 extern int yyparse();
+void show(AS_instr ins) {
+    FG_Showinfo(stdout, ins, Temp_name());
+}
 int main(int argc, const char * argv[]) {
     yyparse();
     typeCheckProg(root);
@@ -28,32 +44,26 @@ int main(int argc, const char * argv[]) {
     while (fl) {
         s=fl->head->stm;
         T_stmList sl = C_linearize(s);
-        fprintf(stdout, "%s\n",fl->head->name);
-        // printStmList_linearized(stdout,sl,0);
         struct C_block c = C_basicBlocks(sl);
-        // printf("%s\n",S_name(c.label));
-        // for (C_stmListList x=c.stmLists;x;x=x->tail){
-        //     printf("**********\n");
-        //     printStmList_linearized(stdout,x->head,0);
-        // }
-        // C_traceSchedule(c);
-        // printStmList_linearized(stdout, C_traceSchedule(c), 0);
-        // c = C_basicBlocks(sl);
-        printf("------~Final traced StmList---------\n");
+        c=C_basicBlocks(C_traceSchedule(c));
         AS_blockList aslist=treep2assemblcok(c);
-        // for (AS_blockList x=aslist;x;x=x->tail){
-        //     printf("**********\n");
-        //     AS_printInstrList(stdout,x->head->instrs,Temp_name());
-        // }
-        // G_nodeList bg=Create_bg(aslist);
-        // printf("------Basic Block Graph---------\n");
-
-        // Show_bg(stdout, bg);
-        AS_instrList il = AS_traceSchedule(aslist, NULL, NULL, FALSE);
-        
+        G_nodeList bg=Create_bg(aslist);
+        printf("------Basic Block Graph---------\n");
+        Show_bg(stdout, bg);
+        printf("------~Final traced StmList---------\n");
+        char des[1010];
+        sprintf(des,"define i64 @%s() #0 {",fl->head->name);
+        AS_instrList prolog=IL(OI(String(des), NULL, NULL ,NULL), NULL);
+        AS_instrList epilog=I(OI("}", NULL, NULL, NULL));
+        AS_instrList il = AS_traceSchedule(aslist, prolog, epilog, FALSE);
         AS_printInstrList(stdout, il, Temp_name());
-        // // fprintf(stdout, "%s\n",fl->head->name);
-        // fprintf(stdout,"\n");
+        G_graph G=FG_AssemFlowGraph(il);
+        G_show(stdout, G_nodes(G), (void*)show);
+        G_nodeList lg=Liveness(G_nodes(G));
+        Show_Liveness(stdout, lg);
+        printf("------Interference Graph---------\n");
+        G_nodeList ig=Create_ig(lg);
+        Show_ig(stdout, ig);
         fl=fl->tail;
     }
     fprintf(stdout, "\n");
